b4f06c66a87eb3783b69b0188f6bb48cac97d3e3
sunnychan <sunnnychan@gmail.com>
2020-06-25 10:31:26 +0800

    sunnychan

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,34 +10,34 @@ module.exports = {
     sidebar: [
       {
         title: '《增长黑客-创业公司的收入和增长秘籍》',   // 必要的
-        path: '/_books/GrowthHacker/',      // 可选的
+        path: '/a-books/GrowthHacker/',      // 可选的
         collapsable: true, // 可选的, 默认值是 true,
         sidebarDepth: 1,    // 可选的, 默认值是 1
         children: [
-          '/_books/GrowthHacker/notes/01.md',
-          '/_books/GrowthHacker/notes/02.md',
-          '/_books/GrowthHacker/notes/03.md',
-          '/_books/GrowthHacker/notes/04.md',
-          '/_books/GrowthHacker/notes/05.md',
-          '/_books/GrowthHacker/notes/06.md',
-          '/_books/GrowthHacker/notes/07.md',
-          '/_books/GrowthHacker/notes/08.md',
+          '/a-books/GrowthHacker/notes/01.md',
+          '/a-books/GrowthHacker/notes/02.md',
+          '/a-books/GrowthHacker/notes/03.md',
+          '/a-books/GrowthHacker/notes/04.md',
+          '/a-books/GrowthHacker/notes/05.md',
+          '/a-books/GrowthHacker/notes/06.md',
+          '/a-books/GrowthHacker/notes/07.md',
+          '/a-books/GrowthHacker/notes/08.md',
         ]
       },
       {
         title: 'GrowingIO',   // 必要的
-        path: '/_books/GrowingIO/',      // 可选的
+        path: '/a-books/GrowingIO/',      // 可选的
         collapsable: true, // 可选的, 默认值是 true,
         sidebarDepth: 1,    // 可选的, 默认值是 1
         children: [
-          '/_books/GrowingIO/CGO/',
-          '/_books/GrowingIO/growth-hacking-handbook/',
-          '/_books/GrowingIO/UBDA/',
+          '/a-books/GrowingIO/CGO/',
+          '/a-books/GrowingIO/growth-hacking-handbook/',
+          '/a-books/GrowingIO/UBDA/',
         ]
       },
       {
         title: '《用户画像》',   // 必要的
-        path: '/_books/user-profile/',      // 可选的
+        path: '/a-books/user-profile/',      // 可选的
         collapsable: true, // 可选的, 默认值是 true,
         sidebarDepth: 1,    // 可选的, 默认值是 1
       },
