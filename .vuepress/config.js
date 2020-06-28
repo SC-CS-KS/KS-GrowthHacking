d89180e0b5b15b45ab8abf5de21f584b8c8ce070
sunnychan <sunnnychan@gmail.com>
2020-06-28 12:25:04 +0800

    sunnychan

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -25,6 +25,44 @@ module.exports = {
         ]
       },
       {
+        title: '《硅谷增长黑客实战笔记》',   // 必要的
+        path: '/a-books/GH-practice-notes/',      // 可选的
+        collapsable: true, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/a-books/GH-practice-notes/notes/01.md',
+          '/a-books/GH-practice-notes/notes/02.md',
+          '/a-books/GH-practice-notes/notes/03.md',
+          '/a-books/GH-practice-notes/notes/04.md',
+          '/a-books/GH-practice-notes/notes/05.md',
+          '/a-books/GH-practice-notes/notes/06.md',
+          '/a-books/GH-practice-notes/notes/07.md',
+          '/a-books/GH-practice-notes/notes/08.md',
+        ]
+      },
+      {
+        title: '《用户增长方法论：找到产品长盛不衰的增长曲线》',   // 必要的
+        path: '/a-books/user-growth-methodology/',      // 可选的
+        collapsable: true, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/a-books/user-growth-methodology/notes/01.md',
+          '/a-books/user-growth-methodology/notes/02.md',
+          '/a-books/user-growth-methodology/notes/03.md',
+          '/a-books/user-growth-methodology/notes/04.md',
+          '/a-books/user-growth-methodology/notes/05.md',
+          '/a-books/user-growth-methodology/notes/06.md',
+          '/a-books/user-growth-methodology/notes/07.md',
+          '/a-books/user-growth-methodology/notes/08.md',
+          '/a-books/user-growth-methodology/notes/09.md',
+          '/a-books/user-growth-methodology/notes/10.md',
+          '/a-books/user-growth-methodology/notes/11.md',
+          '/a-books/user-growth-methodology/notes/12.md',
+          '/a-books/user-growth-methodology/notes/13.md',
+          '/a-books/user-growth-methodology/notes/14.md',
+        ]
+      },
+      {
         title: 'GrowingIO',   // 必要的
         path: '/a-books/GrowingIO/',      // 可选的
         collapsable: true, // 可选的, 默认值是 true,
